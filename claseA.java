1b286f71d4bf64bf34f97c837e9b78c0be14adbe
¨ZiskaS¨ <franziska.stude@gmx.de>
2023-06-20 22:25:32 +0200

    Agregada clase A

---
+++ claseA.java
@@ -0,0 +1,33 @@
+public class ClaseA {
+    private int foo;
+    private String bar;
+    private String lorem;
+
+    public ClaseA(int foo, String bar) {
+        this.foo = foo;
+        this.bar = bar;
+    }
+
+    // Agrega métodos y lógica adicional según sea necesario
+
+    // Ejemplo de un método getter
+    public int getFoo() {
+        return foo;
+    }
+
+    // Ejemplo de un método setter
+    public void setFoo(int foo) {
+        this.foo = foo;
+    }
+
+    // Ejemplo de un método getter para el atributo "lorem"
+    public String getLorem() {
+        return lorem;
+    }
+
+    // Ejemplo de un método setter para el atributo "lorem"
+    public void setLorem(String lorem) {
+        this.lorem = lorem;
+    }
+}
+
---
+++ claseC.java
@@ -0,0 +1,22 @@
+public class ClaseB {
+    private int foo;
+    private String bar;
+
+    public ClaseB(int foo, String bar) {
+        this.foo = foo;
+        this.bar = bar;
+    }
+
+    // Agrega métodos y lógica adicional según sea necesario
+
+    // Ejemplo de un método getter
+    public int getFoo() {
+        return foo;
+    }
+
+    // Ejemplo de un método setter
+    public void setFoo(int foo) {
+        this.foo = foo;
+    }
+}
+
