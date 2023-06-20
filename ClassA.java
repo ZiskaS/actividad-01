4e6d4d39a8f31ef0aad8a6541b6011f50aef2d16
¨ZiskaS¨ <franziska.stude@gmx.de>
2023-06-20 21:48:47 +0200

    Añadir clase A con atributos foo y bar

---
+++ ClassA.java
@@ -0,0 +1,28 @@
+public class ClassA {
+    private String foo;
+    private String bar;
+
+    // Constructor
+    public ClassA(String foo, String bar) {
+        this.foo = foo;
+        this.bar = bar;
+    }
+
+    // Getters and setters
+    public String getFoo() {
+        return foo;
+    }
+
+    public void setFoo(String foo) {
+        this.foo = foo;
+    }
+
+    public String getBar() {
+        return bar;
+    }
+
+    public void setBar(String bar) {
+        this.bar = bar;
+    }
+}
+
